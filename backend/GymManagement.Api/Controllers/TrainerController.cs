2306ea30f8b29edef4c661b5e047f7d65f022e6b
besartakuci-dev <bk73837@ubt-uni.net>
2026-05-17 20:02:56 +0200

    JWT Token, Users Roles

---
+++ backend/GymManagement.Api/Controllers/TrainerController.cs
@@ -0,0 +1,29 @@
+using GymManagement.Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers
+{
+    [Authorize(Roles = $"{Roles.Trainer},{Roles.Admin}")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainerController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TrainerController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var username = User.FindFirst("sub")?.Value;
+            var user = await _userManager.FindByNameAsync(username!);
+            if (user == null) return NotFound();
+            return Ok(new { user.Id, user.UserName });
+        }
+    }
+}
---
+++ backend/GymManagement.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.Api.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
